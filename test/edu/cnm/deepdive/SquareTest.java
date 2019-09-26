8e1fd009c8e0fc31fc9cf34616f82467831b8f94
edubois9119 <erica.dubois9119@gmail.com>
2019-09-26 09:32:38 -0600

    Executable implementation

--- test/edu/cnm/deepdive/SquareTest.java
+++ test/edu/cnm/deepdive/SquareTest.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class SquareTest {
 
@@ -20,11 +21,14 @@ class SquareTest {
 
   @Test
   void isPerfectSquareException() {
-    try{
+    assertThrows(IllegalArgumentException.class, new ExecuteIsPerfectSquare() );
+  }
+
+  private static class ExecuteIsPerfectSquare implements Executable {
+
+    @Override
+    public void execute() throws Throwable {
       Square.isPerfectSquare(-1);
-      fail();
-    } catch (IllegalArgumentException expected) {
-      // Do nothing; this is exactly as expected
     }
   }
 }
